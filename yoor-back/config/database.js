78e8151ed6482d347f47cb7b9cc6a4b41eb3c4d4
jurrgis <63541570+jurrgis@users.noreply.github.com>
2023-01-17 18:59:58 +0200

    Configured production/database.json

--- yoor-back/config/database.js
+++ yoor-back/config/database.js
@@ -1,13 +1,18 @@
+// path: ./config/database.js`
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapiapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'windows98me'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
+    debug: false,
   },
 });
